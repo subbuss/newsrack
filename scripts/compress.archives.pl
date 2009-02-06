9cad7735aa2a905a41870d559d946d9bb477cbea
Subramanya Sastry <sss.lists@gmail.com>
2009-02-06 23:00:31 +0000

    - Updated compression script

--- scripts/compress.archives.pl
+++ scripts/compress.archives.pl
@@ -3,8 +3,15 @@
 # import packages
 use File::Copy;
 
+$numargs  = @ARGV;
+$YEAR     = @ARGV[0] if $numargs > 0;
+$MONTH    = @ARGV[1] if $numargs > 1;
+$D_REGEXP = @ARGV[2] if $numargs > 2;
+
+#$gna  = "/data/newsrack/archive.test";
 $gna  = "/home/newsrack/data/news.archive";
-$gna2 = "home/newsrack/data/news.archive";
+$gna2 = $gna;
+$gna2 =~ s{^/}{};
 $orig = "$gna/orig";
 
 opendir ARCHIVE, "$orig" || die "could not open directory $orig";
@@ -12,21 +19,28 @@ chdir $orig;
 @allYears = grep !/^\.\.?/, readdir ARCHIVE;
 foreach $year (@allYears) {
 	next if -f $year;
-	next if !($year =~ 2008);
+	print "got $year; testing against $YEAR\n";
+	next if $YEAR && !($year == $YEAR);
+
+	print "--- YEAR: $year ---\n";
 
 	opendir YEARDIR, "$year" || die "could not open directory $year";
 	chdir $year;
 	@allMonths = grep !/^\.\.?/, readdir YEARDIR;
 	foreach $month (@allMonths) {
-#		next if -f $month;
-		next if !($month =~ 7);
+		next if -f $month;
+		print "got $month; testing against $MONTH\n";
+		next if $MONTH && !($month == $MONTH);
+
+		print "--- MONTH: $month ---\n";
 
 		opendir MONTHDIR, "$month" || die "could not open directory $month";
 		chdir $month;
 		@allDates = grep !/^\.\.?/, readdir MONTHDIR;
 		foreach $date (@allDates) {
 			next if -f $date;
-# 			next if !($date =~ /^2?[0-6]$/);
+		   print "got $date; testing against $D_REGEXP\n";
+ 			next if $D_REGEXP && !($date =~ /$D_REGEXP/);
 
 			print "--- DATE: $date ---\n";
 			chdir $date || die "could not chdir to $date" ;
@@ -35,37 +49,45 @@ foreach $year (@allYears) {
 			@allSrcs = grep !/^\.\.?/, readdir DATEDIR;
          system("tar xzf home.tgz") if -f "home.tgz";
 			foreach $src (@allSrcs) {
-            next if ($src =~ /home/);
+            next if ($src =~ /^home$/);
+            next if ($src =~ /^var$/);
             if (-f "$src") {
                $src =~ s/.tgz//g;
+
+						# DUE TO SOME BUGS IN EARLIER VERSION OF THE SCRIPT
                $x = "$gna2/orig/$year/$month/$date";
-               $y = "$x";
-               if (-d "$y/$gna2/orig/$year/$month/$date/$src") {
-                  print "NEW SRC: $src\n";
-                  system("mkdir -p $src; mv $y/$gna2/orig/$year/$month/$date/$src/* $src");
-                  system("rm -rf $y/$gna2/orig/$year/$month/$date/$src");
-                  print "EXEC: tar czf '$orig/$year/$month/$date/$src.tgz' '$orig/$year/$month/$date/$src'\n";
-                  system("tar czf '$orig/$year/$month/$date/$src.tgz' '$orig/$year/$month/$date/$src'");
-                  print "EXEC: rm -rf $orig/$year/$month/$date/$src\n" ;
-                  system("rm -rf '$orig/$year/$month/$date/$src'");
+               if (-d "$x/$gna2/orig/$year/$month/$date/$src") {
+                  system("mkdir -p \'$src\'; mv $x/$gna2/orig/$year/$month/$date/$src/* $src");
+                  system("rm -rf $x/$gna2/orig/$year/$month/$date/$src");
+                  print "EXEC: cd $orig/$year/$month/$date; tar czf \'$src.tgz\' \'$src\'\n";
+                  system("cd $orig/$year/$month/$date; tar czf \'$src.tgz\' \'$src\'");
+                  print "EXEC: rm -rf \'$orig/$year/$month/$date/$src\'\n" ;
+                  system("rm -rf \'$orig/$year/$month/$date/$src\'");
                }
             }
             else {
                print "\tSRC: $src ---\n";
 
                if (-f "$src.tgz") {
-                  system("tar xzf $src.tgz; rm -f $src.tgz");
-                  system("mv $gna2/orig/$year/$month/$date/$src/* $src");
-                  system("rm -rf $gna2/orig/$year/$month/$date/$src");
-                  system("mv $y/$gna2/orig/$year/$month/$date/$src/* $src");
-                  system("rm -rf $y/$gna2/orig/$year/$month/$date/$src");
+                  system("tar xzf \'$src.tgz\'; rm -f \'$src.tgz\'");
+
+							# DUE TO SOME BUGS IN EARLIER VERSION OF THE SCRIPT
+						if (-d "$gna2/orig$year/$month/$date/$src") {
+							system("mv \'$gna2/orig/$year/$month/$date/$src/*\' \'$src\'");
+							system("rm -rf \'$gna2/orig/$year/$month/$date/$src\'");
+							$x = "$gna2/orig/$year/$month/$date";
+							if (-d "$x/$gna2/orig/$year/$month/$date/$src") {
+								system("mv \'$x/$gna2/orig/$year/$month/$date/$src/*\' \'$src\'");
+								system("rm -rf \'$x/$gna2/orig/$year/$month/$date/$src\'");
+							}
+						}
                }
 
                   # Compress the original articles into its new destination
-               print "EXEC: tar czf '$orig/$year/$month/$date/$src.tgz' '$orig/$year/$month/$date/$src'\n";
-               system("tar czf '$orig/$year/$month/$date/$src.tgz' '$orig/$year/$month/$date/$src'");
-               print "EXEC: rm -rf $orig/$year/$month/$date/$src\n" ;
-               system("rm -rf '$orig/$year/$month/$date/$src'");
+               print "EXEC: cd $orig/$year/$month/$date; tar czf \'$src.tgz\' \'$src\'\n";
+               system("cd $orig/$year/$month/$date; tar czf \'$src.tgz\' \'$src\'");
+               print "EXEC: rm -rf \'$orig/$year/$month/$date/$src\'\n" ;
+               system("rm -rf \'$orig/$year/$month/$date/$src\'");
             }
 			}
          system("rm -rf home; rm -f xzf home.tgz") if -d "home";
