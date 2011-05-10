8dbdf1864d18d444f6d0651f2a3122e8394f302f
Subramanya Sastry <sss.lists@gmail.com>
2011-05-10 04:53:45 +0000

    update to use tomcat6

--- crawlers/h6.crawlers.sh
+++ crawlers/h6.crawlers.sh
@@ -3,5 +3,5 @@
 for paper in projectsmonitor fline chandigarh.tribune pib business.standard nbt fe #nie pioneer statesman central.chronicle
 do
    echo "---- generating rss for $paper ----"
-   /var/lib/tomcat5.5/webapps/newsrack.crawlers/gen.$paper.rss.pl > /tmp/$paper.out
+   /var/lib/tomcat6/webapps/newsrack.crawlers/gen.$paper.rss.pl > /tmp/$paper.out
 done
--- crawlers/h8.crawlers.sh
+++ crawlers/h8.crawlers.sh
@@ -3,5 +3,5 @@
 for paper in dna.bangalore kanglaonline epao starofmysore midday hindustan.dainik #lebanonfiles journaladdiyar elnashra
 do
    echo "---- generating rss for $paper ----"
-   /var/lib/tomcat5.5/webapps/newsrack.crawlers/gen.$paper.rss.pl > /tmp/$paper.out
+   /var/lib/tomcat6/webapps/newsrack.crawlers/gen.$paper.rss.pl > /tmp/$paper.out
 done
