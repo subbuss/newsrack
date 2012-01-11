7705cc0d1d396401640ea83bc7a96ac5b8fa3869
subbu_ss <subbu_ss@53642c28-9d4c-0410-8ae0-d6925f32bd3b>
2012-01-11 08:02:43 +0000

    Added crawler Hindu to crawl Todays Paper section which is not part of their RSS feeds

--- crawlers/h17.crawlers.sh
+++ crawlers/h17.crawlers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for paper in dainik.jagran assam.tribune 
+for paper in hindu.todays_paper dainik.jagran assam.tribune 
 do
    echo "---- generating rss for $paper ----"
    /var/lib/tomcat6/webapps/newsrack.crawlers/gen.$paper.rss.pl > /tmp/$paper.out
--- crawlers/h6.crawlers.sh
+++ crawlers/h6.crawlers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for paper in projectsmonitor fline chandigarh.tribune pib business.standard nbt fe #nie pioneer statesman central.chronicle
+for paper in hindu.todays_paper projectsmonitor fline chandigarh.tribune pib business.standard nbt fe #nie pioneer statesman central.chronicle
 do
    echo "---- generating rss for $paper ----"
    /var/lib/tomcat6/webapps/newsrack.crawlers/gen.$paper.rss.pl > /tmp/$paper.out
