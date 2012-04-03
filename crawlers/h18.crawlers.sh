8f808ed588f74e269eb3cc857b6213483175fbca
Subramanya Sastry <sss.lists@gmail.com>
2012-04-03 21:54:05 +0530

    Turn off Financial Express crawling

--- crawlers/h18.crawlers.sh
+++ crawlers/h18.crawlers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for paper in toi kanglaonline epao starofmysore central.chronicle pib business.standard fe #nie 
+for paper in toi kanglaonline epao starofmysore central.chronicle pib business.standard #fe nie 
 do
    echo "---- generating rss for $paper ----"
    /var/lib/tomcat6/webapps/newsrack.crawlers/gen.$paper.rss.pl > /tmp/$paper.out
