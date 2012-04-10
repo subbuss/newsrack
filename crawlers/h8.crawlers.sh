6ff6ef951002071003fe77473f41bfd3d3d6b7e1
Subramanya Sastry <sss.lists@gmail.com>
2012-04-10 11:35:03 -0500

    Turned off mid-day and central chronicle crawling

--- crawlers/h8.crawlers.sh
+++ crawlers/h8.crawlers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for paper in dna.bangalore kanglaonline epao starofmysore midday hindustan.dainik #lebanonfiles journaladdiyar elnashra
+for paper in dna.bangalore kanglaonline epao starofmysore hindustan.dainik #midday lebanonfiles journaladdiyar elnashra
 do
    echo "---- generating rss for $paper ----"
    /var/lib/tomcat6/webapps/newsrack.crawlers/gen.$paper.rss.pl > /tmp/$paper.out
