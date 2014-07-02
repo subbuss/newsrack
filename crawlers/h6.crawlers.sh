943a5c20fb43c57b4350420b9de80ab8a9fd4552
Subramanya Sastry <sss.lists@gmail.com>
2014-07-01 23:21:37 -0500

    Turn off projectsmonitor crawler -- it now provides its own RSS feed

--- crawlers/h6.crawlers.sh
+++ crawlers/h6.crawlers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for paper in cgnet hindu.todays_paper projectsmonitor chandigarh.tribune pib business.standard nbt #fline fe nie pioneer statesman central.chronicle
+for paper in cgnet hindu.todays_paper chandigarh.tribune pib business.standard nbt #projectsmonitor fline fe nie pioneer statesman central.chronicle
 do
    echo "---- generating rss for $paper ----"
    /var/lib/tomcat6/webapps/newsrack.crawlers/gen.$paper.rss.pl > /tmp/$paper.out
